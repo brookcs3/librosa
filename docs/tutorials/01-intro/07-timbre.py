0ac26cc1db018e6f53fdc9300ae2a3a6f004c9df
Brian McFee <brian.mcfee@nyu.edu>
2026-02-06 09:29:11 -0500

    filling out

---
+++ docs/tutorials/01-intro/07-timbre.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+"""
+===========================
+Timbre and spectral content
+===========================
+
+This section introduces common tools for representing the timbral or textural
+content of audio signals.
+"""
---
+++ docs/tutorials/01-intro/08-notation.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+"""
+==============
+Music notation
+==============
+
+This section introduces tools for working with various music notational systems, and
+converting between them.
+"""
