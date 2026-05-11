421edcdb4b0a01e7eb64f831cfa68db3460bf655
Brian McFee <brian.mcfee@nyu.edu>
2026-05-11 09:42:26 -0400

    working on docs

--- docs/tutorials/01-intro/07-timbre.py
+++ docs/tutorials/01-intro/07-timbre.py
@@ -7,3 +7,27 @@ Timbre and spectral content
 This section introduces common tools for representing the timbral or textural
 content of audio signals.
 """
+
+# to include:
+# 
+#   - rms energy
+#   - spectral centroid
+#   - spectral bandwidth
+#   - spectral rolloff
+#   - spectral contrast
+#   
+# These can be illustrated using the nutcracker example maybe?
+#  what really would be a good example to illustrate these features
+#  though?
+# 
+# what we need is a recording that shows three different instruments
+# playing three different notes, arpeggiated and then as a chord
+#   the arpeggiation should vary dynamics as well as pitch
+#   probably we can assemble this from tinysol or something
+#   we can then mix in some noise to show how these features change
+
+# then, mfccs
+#  - we can show how they are derived from the mel spectrogram
+#  - plot the DCT basis and explain spectral envelope extraction
+#  - show how pitch variation is captured in the high order coefficients
+
